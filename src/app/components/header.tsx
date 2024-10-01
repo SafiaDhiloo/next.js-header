79f4185d548275bc3a61fb05ddf50d4268b8323e
Safia Dhiloo <safiadiloo@gmail.com>
2024-10-02 00:43:05 +0500

    first commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,19 @@
+
+export default function Header() {
+    return (
+        <nav className="navigation p-0.5 background-color: rgb(8 145 178);">
+            <div >
+                <ul className="header flex items-center justify-center gap-8 bg-stone-600  ">
+                    <li className="list1 text-xl">home</li>
+                    <li className="list1 text-xl">contact Us</li>
+                    <li className="list1 text-xl">about Us</li>
+                    <li className="list1 text-xl">portfolio</li>
+                </ul>
+            </div>
+
+        </nav>
+
+    );
+
+
+}
